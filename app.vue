9b55155c5b1dc4b453d6bb0dc6fa5b1761b2b41b
Dom <maxum245@gmail.com>
2025-04-03 15:45:21 +0300

    feat(pages): створено сторінку зі списком продуктів

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout />
+    <NuxtPage />
   </div>
 </template>
+
+<script setup>
+const route = useRoute()
+
+useHead({
+  title: route.meta.title || 'Мій сайт'
+})
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,11 @@
+<script setup>
+useHead({
+  title: 'Список продуктів'
+})
+</script>
+
+<template>
+  <div>
+    <h1>Список продуктів</h1>
+  </div>
+</template>
